2bb0ec39a733b829cf2f8a9742f79f5552c2fd61
nurlykhan <n.salamatoff@gmail.com>
2020-05-27 09:43:14 +0500

    a

---
+++ client/build/precache-manifest.58ce1fae01c68dc9b94ded64e1cb0e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87387e17c22382ca1ed8f49e51ce1931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce1c48096c72a8bd14e5",
+    "url": "/static/css/main.a350b7ac.chunk.css"
+  },
+  {
+    "revision": "ac4ee25d84a2cf1650bf",
+    "url": "/static/js/2.eab88ca6.chunk.js"
+  },
+  {
+    "revision": "ce1c48096c72a8bd14e5",
+    "url": "/static/js/main.1f5938fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
